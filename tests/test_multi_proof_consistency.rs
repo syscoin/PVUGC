bcdc61f955a16eb2533dab97702e6b3123071529
Mickey Joe <mickey.johnnyson@gmail.com>
2025-10-11 22:31:29 +0200

    Port multi-proof consistency test to current main

--- tests/test_multi_proof_consistency.rs
+++ tests/test_multi_proof_consistency.rs
@@ -1,17 +1,18 @@
-use ark_bls12_381::Fr;
+use ark_bls12_381::{Bls12_381, Fr, Fq12};
 use ark_ec::{AffineRepr, CurveGroup};
 use ark_std::test_rng;
 
 use arkworks_groth16::groth16_wrapper::ArkworksGroth16;
 use arkworks_groth16::gs_commitments::GrothSahaiCommitments;
 
-use groth_sahai::{
-    masked_verifier_matrix_canonical_2x2,
-    masked_verifier_comt_2x2,
+use arkworks_groth16::{
+    masked_verifier_matrix_canonical,
     rhs_masked_matrix,
     kdf_from_comt,
 };
+use groth_sahai::data_structures::{ComT, Matrix};
 use groth_sahai::prover::Provable;
+use ark_ec::pairing::PairingOutput;
 
 #[test]
 fn test_masked_matrix_consistency_and_uniqueness() {
@@ -24,7 +25,7 @@ fn test_masked_matrix_consistency_and_uniqueness() {
     // Statement A: public input 25
     let witness_a = Fr::from(5u64);
     let x_a = vec![Fr::from(25u64)];
-    let ppe_a = gs.groth16_verify_as_ppe_2var(&vk, &x_a);
+    let ppe_a = gs.groth16_verify_as_ppe(&vk, &x_a);
     let crs = gs.get_crs().clone();
 
     let mut rng_a1 = test_rng();
@@ -55,7 +56,7 @@ fn test_masked_matrix_consistency_and_uniqueness() {
     );
 
     let rho = Fr::from(777u64);
-    let lhs_a1 = masked_verifier_matrix_canonical_2x2(
+    let lhs_a1 = masked_verifier_matrix_canonical(
         &ppe_a,
         &crs,
         &cpr_a1.xcoms.coms,
@@ -64,7 +65,7 @@ fn test_masked_matrix_consistency_and_uniqueness() {
         &cpr_a1.equ_proofs[0].theta,
         rho,
     );
-    let lhs_a2 = masked_verifier_matrix_canonical_2x2(
+    let lhs_a2 = masked_verifier_matrix_canonical(
         &ppe_a,
         &crs,
         &cpr_a2.xcoms.coms,
@@ -73,7 +74,7 @@ fn test_masked_matrix_consistency_and_uniqueness() {
         &cpr_a2.equ_proofs[0].theta,
         rho,
     );
-    let lhs_a3 = masked_verifier_matrix_canonical_2x2(
+    let lhs_a3 = masked_verifier_matrix_canonical(
         &ppe_a,
         &crs,
         &cpr_a3.xcoms.coms,
@@ -88,33 +89,9 @@ fn test_masked_matrix_consistency_and_uniqueness() {
     assert_eq!(lhs_a2, rhs_a, "Proof A2 masked matrix must equal target");
     assert_eq!(lhs_a3, rhs_a, "Proof A3 (alternate witness) masked matrix must equal target");
 
-    let comt_a1 = masked_verifier_comt_2x2(
-        &ppe_a,
-        &crs,
-        &cpr_a1.xcoms.coms,
-        &cpr_a1.ycoms.coms,
-        &cpr_a1.equ_proofs[0].pi,
-        &cpr_a1.equ_proofs[0].theta,
-        rho,
-    );
-    let comt_a2 = masked_verifier_comt_2x2(
-        &ppe_a,
-        &crs,
-        &cpr_a2.xcoms.coms,
-        &cpr_a2.ycoms.coms,
-        &cpr_a2.equ_proofs[0].pi,
-        &cpr_a2.equ_proofs[0].theta,
-        rho,
-    );
-    let comt_a3 = masked_verifier_comt_2x2(
-        &ppe_a,
-        &crs,
-        &cpr_a3.xcoms.coms,
-        &cpr_a3.ycoms.coms,
-        &cpr_a3.equ_proofs[0].pi,
-        &cpr_a3.equ_proofs[0].theta,
-        rho,
-    );
+    let comt_a1 = matrix_to_comt(lhs_a1);
+    let comt_a2 = matrix_to_comt(lhs_a2);
+    let comt_a3 = matrix_to_comt(lhs_a3);
 
     let key_a1 = kdf_from_comt(&comt_a1, b"crs", b"ppe", b"vk", b"x", b"deposit", 1);
     let key_a2 = kdf_from_comt(&comt_a2, b"crs", b"ppe", b"vk", b"x", b"deposit", 1);
@@ -125,7 +102,7 @@ fn test_masked_matrix_consistency_and_uniqueness() {
     // Statement B: different public input 36 (witness 6)
     let witness_b = Fr::from(6u64);
     let x_b = vec![Fr::from(36u64)];
-    let ppe_b = gs.groth16_verify_as_ppe_2var(&vk, &x_b);
+    let ppe_b = gs.groth16_verify_as_ppe(&vk, &x_b);
     let proof_b = groth16.prove(witness_b).expect("proof b");
     let mut rng_b = test_rng();
     let cpr_b = ppe_b.commit_and_prove(
@@ -135,7 +112,7 @@ fn test_masked_matrix_consistency_and_uniqueness() {
         &mut rng_b,
     );
 
-    let lhs_b = masked_verifier_matrix_canonical_2x2(
+    let lhs_b = masked_verifier_matrix_canonical(
         &ppe_b,
         &crs,
         &cpr_b.xcoms.coms,
@@ -149,15 +126,14 @@ fn test_masked_matrix_consistency_and_uniqueness() {
 
     // Ensure distinct statements produce different masked matrices/keys
     assert_ne!(lhs_a1, lhs_b, "Different statements should not share masked matrices");
-    let comt_b = masked_verifier_comt_2x2(
-        &ppe_b,
-        &crs,
-        &cpr_b.xcoms.coms,
-        &cpr_b.ycoms.coms,
-        &cpr_b.equ_proofs[0].pi,
-        &cpr_b.equ_proofs[0].theta,
-        rho,
-    );
+    let comt_b = matrix_to_comt(lhs_b);
     let key_b = kdf_from_comt(&comt_b, b"crs", b"ppe", b"vk", b"x", b"deposit", 1);
     assert_ne!(key_a1, key_b, "Different statements should not derive identical KEM keys");
+}
+
+fn matrix_to_comt(matrix: [[Fq12; 2]; 2]) -> ComT<Bls12_381> {
+    ComT::<Bls12_381>::from(Matrix::<PairingOutput<Bls12_381>>::from(vec![
+        vec![PairingOutput::<Bls12_381>(matrix[0][0]), PairingOutput::<Bls12_381>(matrix[0][1])],
+        vec![PairingOutput::<Bls12_381>(matrix[1][0]), PairingOutput::<Bls12_381>(matrix[1][1])],
+    ]))
 }
